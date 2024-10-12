aca5fc8d736d5c0aff8c4bd39ed7e54abab92725
Justin Plummer <justaplummer@users.noreply.github.com>
2024-10-12 09:35:41 -0500

    Dagger throw cooldown
    Added a cooldown timer to the player node so that players cannot spam daggers by mashing the keyboard. This will also set up one of the powerups to be a increase dagger throw speed

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -1,8 +1,10 @@
 extends CharacterBody2D
 @onready var camera_2d: Camera2D = $Camera2D
+@onready var dagger_throw_cooldown: Timer = $Dagger_Throw_Cooldown
 
 @export var dagger_scene : PackedScene
 const SPEED = 200.0
+var can_throw_dagger = true
 	
 func get_input():
 	var input_direction = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
@@ -14,17 +16,17 @@ func _physics_process(_delta):
 	move_and_slide()
 
 func shoot_projectile():
+	if not can_throw_dagger: return
 	if Input.is_action_just_pressed("attack") || Input.is_action_just_pressed("ui_accept"):
-		var dagger = dagger_scene.instantiate()
-		var mouse_rotation = get_local_mouse_position().angle_to(Vector2.RIGHT) * -1
-		dagger.set_rotation_from_player(mouse_rotation,position)
-		get_parent().add_child(dagger)
-#method of movement that blends 8 way and looking with mouse. Might use this later so that we can inlcude aiming.
-#func get_input():
-	#look_at(get_global_mouse_position())
-	#var input_direction = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
-	#velocity = input_direction * SPEED
-#
-#func _physics_process(delta):
-	#get_input()
-	#move_and_slide()
+		add_dagger()
+		can_throw_dagger = false
+		dagger_throw_cooldown.start(0.75)
+		
+func add_dagger():
+	var dagger = dagger_scene.instantiate()
+	var mouse_rotation = get_local_mouse_position().angle_to(Vector2.RIGHT) * -1
+	dagger.set_rotation_from_player(mouse_rotation,position)
+	get_parent().add_child(dagger)
+
+func _on_dagger_throw_cooldown_timeout() -> void:
+	can_throw_dagger = true
